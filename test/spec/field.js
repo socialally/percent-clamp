dd4a957e48679a156e6e443b0f6b352b3d8cf8f2
cyberfunk <hello@cyberfunk.org>
2015-03-26 09:36:45 +0800

    Tidy code, trigger code path.

---
+++ test/spec/field.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect
+  , percent = require('percent');
+
+describe('Percent:', function() {
+
+  it('should use custom property field',
+    function(done) {
+      expect(percent).to.be.a('function');
+      var src = {a: {ratio: 0.13626332},
+        b: {ratio: 0.47989636},
+        c: {ratio: 0.09596008},
+        d: {ratio: 0.28788024},
+        e: {}};
+      var res = percent(src, null, 'val');
+      expect(res).to.be.an('object');
+      // same objects
+      expect(res).to.equal(src);
+      console.log(res);
+      var t = 0, value;
+      var keys = Object.keys(res);
+      keys.map(function(key) {
+        value = res[key];
+        t += value.val ? value.val : 0;
+      })
+      expect(t).to.eql(100);
+      expect(keys).to.eql(Object.keys(src));
+      done();
+    }
+  );
+
+});
--- test/spec/index.js
+++ test/spec/index.js
@@ -2,4 +2,5 @@ module.exports = {
   percent: require('./percent'),
   error: require('./error'),
   range: require('./range'),
+  field: require('./field'),
 }
